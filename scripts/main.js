091ea1e506767d1a503edc81811328d64d69b69b
tinayetsina <tinayetsina@outlook.com>
2022-08-02 13:27:49 +0800

    update - completed awesome books

--- scripts/main.js
+++ scripts/main.js
@@ -19,16 +19,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const bookList = document.querySelector(".books-card");
 
     bookList.innerHTML += `
+    <div class="books-card-flex">
     <h3 class="book-title">${book[0].bookTitle}</h3>
     <p class="book-about">${book[0].bookAuthor}</p>
     <button type="button" class="button-remove">Remove</button>
     <br><hr>
+    </div>
     `;
-    
+
   });
 });
 
+document.addEventListener('DOMContentLoaded', () => {
+  const buttonRemove = document.querySelectorAll('.button-remove');
+  
+  buttonRemove.forEach(button => {
+    button.addEventListener('click', (e) => {
+      const bookAuthor = e.target.previousElementSibling.innerText;
+      const bookTitle = e.target.previousElementSibling.previousElementSibling.innerText;
 
+      for (let i=0; i<books.length; i++) {
+        if (books[i][0].bookTitle == bookTitle && books[i][0].bookAuthor == bookAuthor) {
+          books.splice(i, 1);
+        }
+      }
 
-console.log(books[2][0].bookTitle);
-console.log(books[2][0].bookAuthor);
+      localStorage.clear();
+      localStorage.setItem('books', JSON.stringify(books));
+      e.target.parentElement.remove();
+    });
+  });
+});
