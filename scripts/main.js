347d5b3fa7e5ac7d9d7d4289273a6a7123ef15b1
tinayetsina <tinayetsina@outlook.com>
2022-08-02 10:20:27 +0800

    book addition complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,34 @@
-const addBook = document.querySelector(".add");
+let books;
+if (localStorage.getItem('books') === null) {
+  books = [];
+} else {
+  books = JSON.parse(localStorage.getItem('books'));
+}
 
-addBook.addEventListener('click', () => {
-  alert('button clicked');
+document.querySelector("form").addEventListener('submit', () => {
+  const bookTitle = document.querySelector(".title").value;
+  const bookAuthor = document.querySelector(".author").value;
+
+  const book = [{bookTitle, bookAuthor}];
+  books.push(book);
+  localStorage.setItem('books', JSON.stringify(books));
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+  books.forEach(book => {
+    const bookList = document.querySelector(".books-card");
+
+    bookList.innerHTML += `
+    <h3 class="book-title">${book[0].bookTitle}</h3>
+    <p class="book-about">${book[0].bookAuthor}</p>
+    <button type="button" class="button-remove">Remove</button>
+    <br><hr>
+    `;
+    
+  });
+});
+
+
+
+console.log(books[2][0].bookTitle);
+console.log(books[2][0].bookAuthor);
