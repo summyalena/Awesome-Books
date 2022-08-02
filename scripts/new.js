929eaf51dc089b3781e8dbaa4673b560153c2d6b
Lena <summyalena@gmail.com>
2022-08-02 15:45:36 +0100

    created a class variable

--- scripts/new.js
+++ scripts/new.js
@@ -1,7 +1,26 @@
 // eslint-disable-next-line no-unused-vars
 class books {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
+  constructor() {
+    this.title =  null;
+    this.author = null;
   }
+
+  addBook(){
+    if(!books.title && books.author === null){
+        return;
+    } else {
+        localStorage.
+    }
+}
+
+removeBook(){
+
+}
+}
+
+let newBook  = new books ({});
+
+document.querySelector('form').addEventListener('submit', () => {
+    const bookTitle = document.querySelector('.title').value;
+    const bookAuthor = document.querySelector('.author').value;
 }
