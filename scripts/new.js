50f2432729d66bed565a6462fb847be122028606
tinayetsina <tinayetsina@outlook.com>
2022-08-03 13:58:27 +0800

    update - finished dynamic pages

--- scripts/new.js
+++ scripts/new.js
@@ -73,15 +73,26 @@ document.querySelector('form').addEventListener('submit', () => {
 
 const addNewPage = document.querySelector('.book-adder-section');
 const addListPage = document.querySelector('.book-list-section');
+const contactPage = document.querySelector('.contact-section');
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.display-add-new').addEventListener('click', () => {
-    addListPage.classList.toggle('active');
-    addNewPage.classList.toggle('active');
+    addListPage.classList.add('active');
+    addNewPage.classList.add('active');
+    contactPage.classList.remove('active');
   });
 
   document.querySelector('.display-list').addEventListener('click', () => {
+    contactPage.classList.remove('active');
     addListPage.classList.remove('active');
+    addNewPage.classList.remove('active');
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('.display-contact').addEventListener('click', () => {
+    contactPage.classList.add('active');
+    addListPage.classList.add('active');
     addNewPage.classList.remove('active');
   });
 });
